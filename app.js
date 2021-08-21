759da4263ba5c8c9ea7fe2f0a7e99cd0eca62acf
pjwhite08 <pamelajeanwhite@gmail.com>
2021-08-21 14:22:00 -0500

    some comments and clean-up

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ let clickCount = 0  ;
 //global variable to hold a random number (to be created later)
 let random = null;
 //global variable to hold the trigger to clear the board
-let updateEnd = true;
-
+let updateEnd = false;
 //function to create a random number between two whole numbers
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max-min) + min);
 }
-
 //function to fetch 12 drink images from thecocktaildb
 let getDrinks = function(){
     for(let i=0; i< 12; i++){
@@ -19,103 +17,125 @@ let getDrinks = function(){
             }).then(function(json){
                 //local variable to save the image
                 let drinkImage = json.drinks[0].strDrinkThumb
-                
-                console.log(`1. click count before displayOneDrink: ${clickCount}`)
+                //call the function that creates and displays each drink
                 displayOneDrink(drinkImage)
-                // searchForRobot
-                
-                // drinkTray.push(drinkImage)
-                // console.log(`array of drinks:  ${drinkTray.length}`)
-                // if (drinkTray.length == 12){
-                //     console.log(`drnk tray is full`)
-            
-                // }
             })
-            
-        //.then(function(){
-        // console.log(`last then `)
-        //     })
     }           
 }
-
+//function the create, display, add event listener to each drink
+//has one parameter, drinkImage
+//takes the drink URL as an argument
 let displayOneDrink = function(drinkImage) {
-    console.log(`2. click count in displayOneDrink: ${clickCount}`)
-    //get the original div
+    //grab the outer/ original div 
     let drinkDisplay = document.querySelector("#drinkContainer");
     //create a div for the current drink image
     let currentDiv = document.createElement("div")
-    //add this class name to that div
+    //add the class name "imageContainers" to the new div
     currentDiv.classList.add("imageContainers")
-    //attach the currentDiv (div that will hold one drink image) to drinkDisplay (the original div that will hold all the divs with drinks)
+    //attach the currentDiv (div that will hold one drink image) 
+    //to drinkDisplay (the original div that will hold all the divs with drinks)
     drinkDisplay.appendChild(currentDiv)
     //create an image tag
     let currentImg = document.createElement("img")
-    //assign the current URL element of the drinkTray array to the img we just created
+    //assign the current URL element to the img just created
     currentImg.src = drinkImage
-    //assign the className drinkImg to that img
+    //assign the className "drinkImg" to that img
     currentImg.className = "drinkImg"
     //attach the image to the recently created div
     currentDiv.appendChild(currentImg)
-
+    //add an event listenter to the div with an image in it
     currentDiv.addEventListener("click", function(){
-        console.log(`3 click count inside event listener: ${clickCount}`)
-        //clickToRemove removes current image, 
+        //function removes current image
+        //and triggers other changes based on click count
         clickToRemove(currentImg, currentDiv)
-        // moveOn(currentImg, currentDiv)
+        // function increasingl blurs the images given clicks
         clickToBlur(currentImg)
     })
 }
-
-
-//this function is a bit like frankenstein - it's all pieced together and definitely not just removing images
+//the main purpose of this function is to remove the images
+//removing the images triggers a click to be counted
+//and this function also does several other things 
+//it's a bit like frankenstein, and I don't really like it
+//but I couldn't think of a logical way to split it up into separate functions
 let clickToRemove = function(currentImg, currentDiv){
-    console.log("i've been clicked and clickToRemove")
-    console.log(`clickCount inside clicktoRemove: ${clickCount}`)
-    // let currentDiv = document.querySelector(".imageContainers") 
-            //create and update a random number upon the first click
-            if (clickCount == 0) {
-                console.log(`clickToRemove called`)
-                random = getRandomInt(1, 12)
-              
-                console.log(`Random number created: ${random}`)
-                 }
-            //when you make the randomly assigned number of clicks... 
-            if (random == clickCount){
-               
-                // prepareRobot(currentDiv, currentImg)
-                //change img to Robot
-                prepareRobot(currentImg, currentDiv)
-                showRobot()
-                // displayEndMessage()
-                // updateEnd = false
-                }
-            //Because previously clicked images were given the class "noDisplay", if you click on an empty space, nothing happens
-            if (currentImg.className == "noDisplay") {
-                return false
-                }
-            if (updateEnd == false){
-                    document.getElementById("end").style.opacity = "0%"
-                    clearBoard()
-                    getDrinks()
-                } 
-            else {currentImg.className = "noDisplay"
-            clickCount = clickCount + 1
-            console.log(clickCount)
-            return clickCount
-             }   
-    }
-let moveOn = function(currentDiv, currentImg) {
-    if (random == clickCount){
-        console.log(`moveOne`)
-        console.log(`random click count reached`)
-    
-        prepareRobot(currentDiv, currentImg)
-        //change img to Robot
-        // showRobot(currentDiv, currentImg)//I moved this to a new then
-        // displayEndMessage()
-        // updateEnd = false
+    //create and update a random number upon the first click
+    if (clickCount == 0) {
+        //after the first click, but before it is counted (later in this function)
+        //a random number between 1 and 12 is created
+        //this random number will be used to trigger the robot to display
+        random = getRandomInt(1, 12)
         }
-}
+    //when the number of clicks equals that random number...
+    if (random == clickCount){
+        //call the function that creates the robot
+        prepareRobot(currentImg, currentDiv)
+        //call the function that displays the robot
+        showRobot()
+        }
+    //Because previously clicked images were given the class "noDisplay", 
+    //if you click on an empty space, nothing happens
+    if (currentImg.className == "noDisplay") {
+        return false
+        }
+    //update end is changed from false to true after the robot is displayed
+    //so the click after the robot is displayed will trigger update end
+    if (updateEnd == true){
+        //call the function that clears the board
+        clearBoard()
+        //call the function that resets the board
+        getDrinks()
+        } 
+    else {
+        //sets the current image's class to "noDisplay"
+        //which sets the opacity to 0% so it can't be seen
+        currentImg.className = "noDisplay"
+        //increments the click count by 1
+        clickCount = clickCount + 1
+        }   
+    }
+let clickToBlur = function(currentDiv){
+    //grab all images
+    let allImg = document.querySelectorAll(".drinkImg")
+    //for each image
+    for (let i = 0; i< allImg.length; i++){
+        let blur = clickCount*0.3
+            //     if (clickCount == 1) {
+            //         allImg[i].style.filter = "blur(0.3px)"
+            //     }
+            //     if (clickCount == 2) {
+            //         allImg[i].style.filter = "blur(0.6px)" 
+            //     }
+            //     if (clickCount == 3) {
+            //         allImg[i].style.filter = "blur(1px)"
+            //     }
+            //     if (clickCount == 4) {
+            //         allImg[i].style.filter = "blur(1.3px)"
+            //     }
+            //     if (clickCount == 5) {
+            //         allImg[i].style.filter = "blur(1.6px)"
+            //     }
+            //     if (clickCount == 6) {
+            //         allImg[i].style.filter = "blur(2px)"
+            //     }
+            //     if (clickCount == 7) {
+            //         allImg[i].style.filter = "blur(2.3px)"
+            //     }
+            //     if (clickCount == 8) {
+            //         allImg[i].style.filter = "blur(2.6px)"
+            //     }
+            //     if (clickCount == 9) {
+            //         allImg[i].style.filter = "blur(3px)"
+            //     }
+            //     if (clickCount == 10) {
+            //         allImg[i].style.filter = "blur(3.3px)"
+            //     }
+            //     if (clickCount == 11) {
+            //         allImg[i].style.filter = "blur(3.6px)"
+            //     }
+            // }
+            
+           
+//funciton that takes the current image and current div as arguments
 let prepareRobot = function(currentImg, currentDiv) {
     console.log(`they match`)
                 ///set the current image id to robotId so that ---
@@ -150,7 +170,7 @@ let showRobot = function(currentImg){
             let currentDiv = document.getElementById("found")
             currentDiv.appendChild(endMessage)
             endMessage.style.opacity = "100%"
-            updateEnd = false
+            updateEnd = true
             // updateEnd = true
             console.log(updateEnd)
             return true
@@ -169,51 +189,7 @@ let showRobot = function(currentImg){
 // }
 
 
-let clickToBlur = function(currentDiv){
-    // let currentDiv = document.querySelectorAll(".imgageContainers")
-    // currentDiv.addEventListener("click", function(){
-        console.log(`click to blur called`)
-        //get all images
-        let allImg = document.querySelectorAll(".drinkImg")
-        console.log(`all img: ${allImg}`)
-        //for each image
-        for (let i = 0; i< allImg.length; i++){
-            if (clickCount == 1) {
-                allImg[i].style.filter = "blur(0.3px)"
-            }
-            if (clickCount == 2) {
-                allImg[i].style.filter = "blur(0.6px)" 
-            }
-            if (clickCount == 3) {
-                allImg[i].style.filter = "blur(1px)"
-            }
-            if (clickCount == 4) {
-                allImg[i].style.filter = "blur(1.3px)"
-            }
-            if (clickCount == 5) {
-                allImg[i].style.filter = "blur(1.6px)"
-            }
-            if (clickCount == 6) {
-                allImg[i].style.filter = "blur(2px)"
-            }
-            if (clickCount == 7) {
-                allImg[i].style.filter = "blur(2.3px)"
-            }
-            if (clickCount == 8) {
-                allImg[i].style.filter = "blur(2.6px)"
-            }
-            if (clickCount == 9) {
-                allImg[i].style.filter = "blur(3px)"
-            }
-            if (clickCount == 10) {
-                allImg[i].style.filter = "blur(3.3px)"
-            }
-            if (clickCount == 11) {
-                allImg[i].style.filter = "blur(3.6px)"
-            }
-        }
-        
-       
+
         
 
 //         //change style using JS
